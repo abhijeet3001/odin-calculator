72fe82fdd4306c63a6079e8b6304b4c14d2261f2
Abhijeet Singh <89379140+abhijeet3001@users.noreply.github.com>
2023-04-14 12:02:12 +0530

    Calculate using operator

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-function add(a,b){
-    return a+b;
+let firstNo;
+let secondNo;
+let operator;
+
+function add(a, b) {
+  return a + b;
 }
-function subtract(a,b){
-    return a-b;
+function subtract(a, b) {
+  return a - b;
 }
-function multiply(a,b){
-    return a*b;
+function multiply(a, b) {
+  return a * b;
 }
-function divide(a,b){
-    return a/b;
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(first, operator, second) {
+  if (operator === "+") return add(first, second);
+  if (operator === "-") return subtract(first, second);
+  if (operator === "*") return multiply(first, second);
+  if (operator === "/") return divide(first, second);
 }
